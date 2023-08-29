5d843a26ec07771cd9072e6b3a38f7761aa5b2d5
Godedict <tolugeorges@gmail.com>
2023-08-29 17:34:46 -0500

    create html skeleton

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const nextPageLink = document.getElementById('nextPageLink');
-const arrowIcon = document.getElementById('arrowIcon');
+document.addEventListener('DOMContentLoaded', () => {
+  const nextPageLinks = document.querySelectorAll('.pagination-next');
 
-nextPageLink.addEventListener('click', () => {
-  nextPageLink.classList.add('slide-animation');
-  arrowIcon.classList.add('slide-animation');
+  nextPageLinks.forEach((nextPageLink) => {
+    nextPageLink.addEventListener('click', function onNextPageLinkClick(event) {
+      event.preventDefault();
 
-  setTimeout(() => {
-    nextPageLink.classList.remove('slide-animation');
-    arrowIcon.classList.remove('slide-animation');
-  }, 500);
+      const arrowIcon = this.querySelector('.arrow-icon');
+
+      arrowIcon.classList.add('slide-animation');
+
+      setTimeout(() => {
+        arrowIcon.classList.remove('slide-animation');
+      }, 500);
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const prevButton = document.querySelector('.prev-button');
+  const nextButton = document.querySelector('.next-button');
+  const carouselItems = document.querySelectorAll('.project-carousel-item');
+
+  let currentIndex = 0;
+
+  function showItem(index) {
+    carouselItems.forEach((item) => item.classList.remove('active'));
+    carouselItems[index].classList.add('active');
+  }
+
+  prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    showItem(currentIndex);
+  });
+
+  nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    showItem(currentIndex);
+  });
+
+  // Show the initial item
+  showItem(currentIndex);
 });
